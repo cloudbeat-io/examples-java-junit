f598b142096653f12b6450d1e3b24bbd4afde145
N. Dimer <ndimer@cloudbeat.io>
2026-02-20 10:35:38 +0100

    Add CbLink usage example.

--- src/test/java/io/cloudbeat/tests/ExampleTest.java
+++ src/test/java/io/cloudbeat/tests/ExampleTest.java
@@ -1,11 +1,10 @@
 package io.cloudbeat.tests;
 
+import io.cloudbeat.common.annotation.CbLink;
+import io.cloudbeat.common.annotation.CbXray;
 import io.cloudbeat.junit.CbJunitExtension;
 import io.cloudbeat.pages.LoginPage;
-import io.cloudbeat.utils.DriverManager;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.WebDriver;
 
 public class ExampleTest extends BaseTest {
     private LoginPage loginPage;
@@ -17,6 +16,8 @@ public class ExampleTest extends BaseTest {
 
     @Test
     @DisplayName("Example of adding test output data/attributes")
+    @CbLink("JIRA-PROJ-1234")
+    @CbLink(value = "PROJ-555", source = CbLink.SRC_XRAY)
     public void OutputDataExample() {
         loginPage.open();
         loginPage.assertPageOpen();
@@ -26,6 +27,10 @@ public class ExampleTest extends BaseTest {
     }
 
     @Test
+    @Tag("hello")
+    @Tag("ups")
+    @CbLink("XRAY:PROJ-666")
+    @CbXray("PROJ-777")
     @DisplayName("Example of getting environment variables from CB")
     public void EnvExample() {
         loginPage.open();
