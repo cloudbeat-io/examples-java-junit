b4562ddd86df7a558e517c4dbd353049e62c7266
romovs <romovs@gmail.com>
2026-09-10 10:38:05 +0300

    Enable har capture

--- src/main/java/io/cloudbeat/utils/DriverManager.java
+++ src/main/java/io/cloudbeat/utils/DriverManager.java
@@ -3,7 +3,10 @@ package io.cloudbeat.utils;
 import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.cloudbeat.junit.CbJunitExtension;
+
+import java.util.Map;
 
 public class DriverManager {
     private static WebDriver driver;
@@ -11,7 +14,15 @@ public class DriverManager {
     public static WebDriver getDriver() {
         if (driver == null) {
             WebDriverManager.chromedriver().clearDriverCache().clearResolutionCache().setup();
-            var chromeDriver = new ChromeDriver();
+            // must be set before the session is created - CbJunitExtension.getCapabilities()
+            // enables Chrome's "performance" log type (goog:loggingPrefs) when running under
+            // CloudBeat, which getHarLog() depends on for HAR capture; wrapping the driver
+            // afterward (as this used to do) is too late for that capability to take effect
+            ChromeOptions options = new ChromeOptions();
+            Map<String, Object> cbCapabilities = CbJunitExtension.getCapabilities();
+            if (cbCapabilities != null)
+                cbCapabilities.forEach(options::setCapability);
+            var chromeDriver = new ChromeDriver(options);
             chromeDriver.manage().window().maximize();
             driver = CbJunitExtension.wrapWebDriver(chromeDriver);
         }
